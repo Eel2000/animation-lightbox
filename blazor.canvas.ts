a765ae585f72109d9bffeffa10cfed90833b3f61
Iida Hiroki <minoru113@hotmail.com>
2020-09-22 23:05:26 +0900

    オニオンスキンの機能を追加

--- blazor.canvas.ts
+++ blazor.canvas.ts
@@ -1,10 +1,16 @@
 ﻿class Color {
-    public R: number = 0;
-    public G: number = 0;
-    public B: number = 0;
-    public Alpha: number = 1.0;
+    public r: number = 0;
+    public g: number = 0;
+    public b: number = 0;
+    public alpha: number = 1.0;
+    constructor (r:number, g:number, b:number, alpha:number = 255){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.alpha = alpha;
+    }
     public toString = () : string => {
-        return `rgba(${this.R},${this.G},${this.B},${this.Alpha})`
+        return `rgba(${this.r},${this.g},${this.b},${this.alpha})`
     } 
 }
 
@@ -24,65 +30,103 @@ class InternalImageData {
 }
 
 class Canvas {
-    public Element : HTMLCanvasElement;
-    public Id : string;
-    public Context : CanvasRenderingContext2D
-    public constructor (id :string)
+    public element : HTMLCanvasElement;
+    public id : string;
+    public context : CanvasRenderingContext2D
+    public color : Color;
+    public constructor (id :string,color:Color = new Color(0,0,0))
     {
-        this.Id = id;
-        this.Element = document.getElementById(id) as HTMLCanvasElement;
-        this.Context = this.Element.getContext(`2d`);
+        this.id = id;
+        this.element = document.getElementById(id) as HTMLCanvasElement;
+        this.context = this.element.getContext(`2d`);
+        this.color = color;
+    }
+
+    get width() :number {return this.element.width;}
+    get height() : number {return this.element.height;}
+
+    public clear() {
+        this.context.clearRect(0,0, this.width,this.height);
+    }
+
+    public getImageData() {
+        return this.context.getImageData(0,0,this.width,this.height);
     }
 }
-var canvas: Canvas;
+
+/// [0] : 編集用のcanvas
+/// [1] : 前フレームのcanvas
+/// [2] : 前々フレームのcanvas
+var canvas: Canvas[] = [];
 
 function setCanvasElement(id: string) {
-    canvas = new Canvas(id);
-    ImageList.push(canvas.Context.getImageData(0,0,canvas.Element.width,canvas.Element.height));
-    //console.log(`${canvas.Element.offsetLeft},${canvas.Element.offsetTop}`);
+    canvas.push(new Canvas(id));
+    canvas.push(new Canvas(id+`-prev`, new Color(255,0,255)));
+    canvas.push(new Canvas(id+`-prev2`, new Color(0,255,255)));
+    ImageList.push(canvas[0].getImageData());
+    //console.log(`${canvas.element.offsetLeft},${canvas.element.offsetTop}`);
 }
 
-function getCanvasPosition(){
-    const rect = canvas.Element.getBoundingClientRect();
+function getCanvasPosition(canvasNumber : number = 0){
+    const rect = canvas[canvasNumber].element.getBoundingClientRect();
     const p:[number,number] = [rect.left, rect.top]
     return p;
 }
 
-function drawStart(x:number,y:number){
-    canvas.Context.beginPath();
-    canvas.Context.moveTo(x,y);
+function clearCanvas(canvasNumber : number = 0){
+    canvas[canvasNumber].clear();
 }
 
-function drawLine(x:number, y:number){
-    canvas.Context.lineTo(x,y);
-    canvas.Context.stroke();
+function drawStart(x:number,y:number,canvasNumber:number = 0){
+    canvas[canvasNumber].context.beginPath();
+    canvas[canvasNumber].context.moveTo(x,y);
 }
 
-function drawEnd(x:number,y:number){
-    canvas.Context.lineTo(x,y);
-    canvas.Context.stroke();
-    canvas.Context.closePath();
+function drawLine(x:number, y:number,canvasNumber:number = 0){
+    canvas[canvasNumber].context.lineTo(x,y);
+    canvas[canvasNumber].context.stroke();
 }
 
-function saveImageData(n: number){
-    const img = canvas.Context.getImageData(0,0,canvas.Element.width,canvas.Element.height);
+function drawEnd(x:number,y:number,canvasNumber:number = 0){
+    canvas[canvasNumber].context.lineTo(x,y);
+    canvas[canvasNumber].context.stroke();
+    canvas[canvasNumber].context.closePath();
+}
+
+function saveImageData(n: number,canvasNumber:number = 0){
+    const img = canvas[canvasNumber].getImageData();
     ImageList[n] = img
     
     return ImageList.length;
 }
 
 function createNewFrame(currentFrame : number){
-    var ret = saveImageData(currentFrame);
-    clearCanvas();
+    let ret = saveImageData(currentFrame);
+    setPreviousFrame(ret,1);
+    setPreviousFrame(ret,2);
+    canvas[0].clear();
     return ret+1;
 }
 
-function setImageData(n: number){
-    canvas.Context.putImageData(ImageList[n],0,0);
+function setImageData(n: number, canvasNumber:number = 0){
+    canvas[canvasNumber].context.putImageData(ImageList[n],0,0);
+    setPreviousFrame(n,1);
+    setPreviousFrame(n,2);
 }
 
-function clearCanvas(){
-    canvas.Context.clearRect(0,0,canvas.Element.width,canvas.Element.height);
+function setPreviousFrame(n: number,times: number = 1){
+    canvas[times].clear();
+    console.log(`${n},${ImageList.length}`);
+    if (n-times > ImageList.length || n<times) return;
+    let prevImagedata = new ImageData(ImageList[n-times].data.slice(),ImageList[n-times].width,ImageList[n-times].height);
+    let prevData = prevImagedata.data;
+    for(var i=0;i<prevData.length; i+=4 ){
+        prevData[i] = canvas[times].color.r;
+        prevData[i+1] = canvas[times].color.g;
+        prevData[i+2] = canvas[times].color.b;
+        //prevData[i+3] = canvas[times].color.alpha;
+    }
+    canvas[times].context.putImageData(prevImagedata,0,0);
 }
 
 var animationCounter = 0;
@@ -91,17 +135,22 @@ function animate(handle :TimerHandler){
         animationCounter = 0;
     }
     
-    canvas.Context.putImageData(ImageList[animationCounter],0,0);
+    canvas[0].context.putImageData(ImageList[animationCounter],0,0);
     animationCounter++;
 }
 
 function startAnimation(){
     console.log("Start Animation");
+    for(let i=0;i<canvas.length;i++){
+        canvas[i].clear();
+    }
     return window.setInterval(animate,1/8 * 1000);
 }
 
 function stopAnimation(intervalId :number){
     console.log(`Stop Animation counter:${animationCounter}`);
     window.clearInterval(intervalId);
+    setPreviousFrame(animationCounter-1,1);
+    setPreviousFrame(animationCounter-2,2);
     return animationCounter-1;
 }
